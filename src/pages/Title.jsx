4088d9b3509a1bb12c52ff0dbadd1d2dbb00a02d
Yossan103 <whiplash.4416@gamil.com>
2025-08-25 16:28:54 +0900

    titlePageのHTMLを作成

---
+++ src/pages/Title.jsx
@@ -0,0 +1,15 @@
+function Title() {
+    return (
+        <div className="titlePage">
+            <p className="title">旅のおもいで</p>
+            <div className="buttonContainer">
+              <button className="linkButton">これまでのきろく</button>
+              <button className="linkButton">きろくを残す</button>
+            </div>  
+        </div>
+    )
+    
+
+
+}
+export default Title;
