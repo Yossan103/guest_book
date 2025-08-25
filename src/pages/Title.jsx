df290172f9acf44f22d7c57e272cca561438663e
Yossan103 <whiplash.4416@gamil.com>
2025-08-25 17:34:04 +0900

    ページ遷移の実装

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,15 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function Title() {
     return (
         <div className="titlePage">
             <p className="title">旅のおもいで</p>
             <div className="buttonContainer">
-              <button className="linkButton">これまでのきろく</button>
-              <button className="linkButton">きろくを残す</button>
+              <Link to='/note'><button className="linkButton">これまでのきろく</button></Link>
+              <Link to='/view'><button className="linkButton">きろくを残す</button></Link>
             </div>  
         </div>
     )
-    
-
-
 }
 export default Title;
