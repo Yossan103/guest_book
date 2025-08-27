65134e9265b68ce29a0441291528629ffaaa0998
MIO <a25dc002am@g.sugiyama-u.ac.jp>
2025-08-27 22:11:29 +0900

    ボタンの改良

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
-import { Button } from "@chakra-ui/react"
+import { Button, Grid ,GridItem } from "@chakra-ui/react"
+import '../styles/Tittle.css';
 
 function Title() {
     return (
         <div className="titlePage">
             <p className="title">旅のおもいで</p>
-            <div className="buttonContainer">
-              <Link to='/note'><Button colorPalette='orange'>これまでのきろく</Button></Link>
-              <Link to='/view'><Button colorPalette='orange'>きろくを残す</Button></Link>
-            </div>  
+            <Grid templateColumns="repeat(2, 1fr)" gap="6">
+                <GridItem colSpan={1}>
+                    <Link to='/note' ><Button colorPalette='orange' className="titleButton">これまでのきろく</Button></Link>
+                </GridItem>
+                <GridItem colSpan={1}>
+                    <Link to='/view'><Button colorPalette='orange' className="titleButton">きろくを残す</Button></Link>
+                </GridItem>
+            </Grid>  
         </div>
     )
 }
