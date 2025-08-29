19379a397ecbd9807cbcce385d2b54306ffcd2da
Yossan103 <whiplash.4416@gamil.com>
2025-08-29 17:34:55 +0900

    文字入力欄　完成

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,6 +1,63 @@
 function Note() {
-    return (
-        <p>Note</p>
-    )
-}
+    return(
+        <div>
+            <form action=''>
+                <div>
+                    <label htmlFor='name'>
+                        名前
+                    </label>
+                    <input
+                        id='name'
+                        type='text'
+                        placeholder='ここに名前を記入'
+                    />
+                </div>
+                <div>
+                    <label htmlFor='title'>
+                        旅のタイトル
+                    </label>
+                    <input
+                        id='title'
+                        type='text'
+                        placeholder='ここにタイトルを記入'
+                    />
+                </div>
+                <div>
+                    <label htmlFor='purpose'>
+                        旅の目的
+                    </label>
+                    <input
+                        id='purpose'
+                        type='text'
+                        placeholder='ここに旅の目的を記入'
+                    />
+                </div>
+                <div>
+                    <label htmlFor='advice'>
+                        旅のアドバイス
+                    </label>
+                    <input
+                        id='advice'
+                        type='text'
+                        placeholder='ここに旅のアドバイスを記入'
+                    />
+                </div>
+                  <div>
+                    <label htmlFor='review'>
+                        旅の感想
+                    </label>
+                    <input
+                        id='review'
+                        type='text'
+                        placeholder='ここに旅の感想を記入'
+                    />
+                </div>
+
+               
+            </form>
+        </div>
+      
+      
+    );
+};
 export default Note;
