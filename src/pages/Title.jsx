a92c94447f30df262421899cf514cf8868f7cbb3
MIO <a25dc002am@g.sugiyama-u.ac.jp>
2025-08-31 04:18:26 +0900

    画像の挿入

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -8,8 +8,8 @@ function Title() {
 
         <div style={{width: 390, height: 844, position: 'relative', background: '#F4ECE5', overflow: 'hidden'}}>
         <div style={{width: 251, height: 251, left: 231, top: -24, position: 'absolute', background: '#EB5C5E', borderRadius: 9999}} />
-        <img style={{width: 133, height: 142, left: -8, top: 49, position: 'absolute', opacity: 0.70}} src="https://placehold.co/133x142" />
-        <img style={{width: 274, height: 274, left: 126, top: 410, position: 'absolute', opacity: 0.40}} src="https://placehold.co/274x274" />
+        <img style={{width: 133, height: 142, left: -8, top: 49, position: 'absolute', opacity: 0.70}} src="/himo.png" />
+        <img style={{width: 274, height: 274, left: 126, top: 410, position: 'absolute', opacity: 0.40}} src="/Torii.png" />
         <div style={{width: 203, height: 203, left: 163, top: 51, position: 'absolute', borderRadius: 9999, border: '2px #EB5C5E solid'}} />
         <div style={{width: 102, height: 102, left: 330, top: 101, position: 'absolute', borderRadius: 9999, border: '2px white solid'}} />
         <div style={{width: 251, height: 251, left: 204.40, top: 917.84, position: 'absolute', transform: 'rotate(176deg)', transformOrigin: 'top left', background: '#EB5C5E', borderRadius: 9999}} />
@@ -28,10 +28,9 @@ function Title() {
                 </GridItem>
             </Grid>  
         </div>
-        <img style={{width: 56.52, height: 56.52, left: 179.24, top: 154.29, position: 'absolute', transform: 'rotate(70deg)', transformOrigin: 'top left', background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.20) 100%)', backgroundBlendMode: 'normal, darken', borderRadius: 9999}} src="https://placehold.co/57x57" />
-        <img style={{width: 147.60, height: 110.70, left: 115.50, top: 149.62, position: 'absolute', transform: 'rotate(-16deg)', transformOrigin: 'top left', background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.20) 100%)', backgroundBlendMode: 'normal, darken'}} src="https://placehold.co/148x111" />
-        <img style={{width: 100.15, height: 100.15, left: 257.10, top: 191.29, position: 'absolute', transform: 'rotate(70deg)', transformOrigin: 'top left', background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.20) 100%)', backgroundBlendMode: 'normal, darken', borderRadius: 9999}} src="https://placehold.co/100x100" />
-        <img style={{width: 261.52, height: 196.14, left: 144.16, top: 183.01, position: 'absolute', transform: 'rotate(-16deg)', transformOrigin: 'top left', background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.20) 100%)', backgroundBlendMode: 'normal, darken'}} src="https://placehold.co/262x196" />
+        {/* <img style={{width: 56.52, height: 56.52, left: 179.24, top: 154.29, position: 'absolute', transform: 'rotate(70deg)', transformOrigin: 'top left', background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.20) 100%)', backgroundBlendMode: 'normal, darken', borderRadius: 9999}} src="/Torii.png"/> */}
+        {/* <img style={{width: 100.15, height: 100.15, left: 257.10, top: 191.29, position: 'absolute', transform: 'rotate(70deg)', transformOrigin: 'top left', background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.20) 0%, rgba(255, 255, 255, 0.20) 100%)', backgroundBlendMode: 'normal, darken', borderRadius: 9999}} src="/jamijami.png" /> */}
+
         </div>
     )
 }
