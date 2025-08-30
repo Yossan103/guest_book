88032fc09624429f53ed98e39593797d5c536713
MIO <a25dc002am@g.sugiyama-u.ac.jp>
2025-08-31 03:37:07 +0900

    ページネーションの配置

--- src/pages/Title.jsx
+++ src/pages/Title.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { Button, Grid ,GridItem } from "@chakra-ui/react"
 import '../styles/Tittle.css';
 
+
 function Title() {
     return (
         <div className="titlePage">
@@ -14,6 +15,11 @@ function Title() {
                     <Link to='/view'><Button colorPalette='orange' className="titleButton">きろくを残す</Button></Link>
                 </GridItem>
             </Grid>  
+            <svg xmlns="http://www.w3.org/2000/svg" width="195" height="167" viewBox="0 0 195 167" fill="none">
+                    <circle cx="69.5" cy="125.5" r="125.5" transform="rotate(175.531 69.5 125.5)" fill="#EB5C5E"/>
+                     width="203" height="203" viewBox="0 0 203 203" fill="none"
+                    <circle cx="101.5" cy="101.5" r="100.5" stroke="#EB5C5E" stroke-width="2"/>
+            </svg>
         </div>
     )
 }
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,6 +1,40 @@
+import {
+    ButtonGroup,
+    For,
+    IconButton,
+    Pagination,
+    Stack,
+  } from "@chakra-ui/react"
+  import { LuChevronLeft, LuChevronRight } from "react-icons/lu"
+
+import '../styles/View.css';  
 function View() {
     return (
-        <p>view</p>
+        <div className="pageSize">
+            <Pagination.Root count={20} pageSize={1} defaultPage={1}>
+              <ButtonGroup variant="ghost" size={"xs"}>
+                <Pagination.PrevTrigger asChild>
+                  <IconButton>
+                    <LuChevronLeft />
+                  </IconButton>
+                </Pagination.PrevTrigger>
+  
+                <Pagination.Items
+                  render={(page) => (
+                    <IconButton variant={{ base: "ghost", _selected: "outline" }}>
+                      {page.value}
+                    </IconButton>
+                  )}
+                />
+  
+                <Pagination.NextTrigger asChild>
+                  <IconButton>
+                    <LuChevronRight />
+                  </IconButton>
+                </Pagination.NextTrigger>
+              </ButtonGroup>
+            </Pagination.Root>
+        </div>
     )
 }
 export default View;
