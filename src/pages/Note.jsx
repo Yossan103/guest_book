1926eceb351b39b2c94a60f9a43b871993c64211
MIO <a25dc002am@g.sugiyama-u.ac.jp>
2025-08-31 09:53:28 +0900

    ノートの行間整頓

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -38,72 +38,75 @@ function Note() {
         // <div className="backGround">
         <div style={{width: "100vw", height: "100vh", position: 'relative', background: '#F4ECE5', overflow: 'hidden'}}>
             <div>
-                <BackButton />
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className='noteContent'>
-                        <div>
-                            <label htmlFor='name'>
-                                名前
-                            </label>
-                            <input
-                                id='name'
-                                type='text'
-                                placeholder='ここに名前を記入'
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
+                <div className="labelName">
+                    <BackButton />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='noteContent'>
+                            <div>
+                                <label htmlFor='name'>
+                                    名前
+                                </label>
+                                <br/>
+                                <input
+                                    id='name'
+                                    type='text'
+                                    placeholder='ここに名前を記入'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='title'>
+                                    旅のタイトル
+                                </label>
+                                <input
+                                    id='title'
+                                    type='text'
+                                    placeholder='ここにタイトルを記入'
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='purpose'>
+                                    旅の目的
+                                </label>
+                                <input
+                                    id='purpose'
+                                    type='text'
+                                    placeholder='ここに旅の目的を記入'
+                                    value={purpose}
+                                    onChange={(e) => setPurpose(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='advice'>
+                                    旅のアドバイス
+                                </label>
+                                <input
+                                    id='advice'
+                                    type='text'
+                                    placeholder='ここに旅のアドバイスを記入'
+                                    value={advice}
+                                    onChange={(e) => setAdvice(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='review'>
+                                    旅の感想
+                                </label>
+                                <input
+                                    id='review'
+                                    type='text'
+                                    placeholder='ここに旅の感想を記入'
+                                    value={review}
+                                    onChange={(e) => setReview(e.target.value)}
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor='title'>
-                                旅のタイトル
-                            </label>
-                            <input
-                                id='title'
-                                type='text'
-                                placeholder='ここにタイトルを記入'
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='purpose'>
-                                旅の目的
-                            </label>
-                            <input
-                                id='purpose'
-                                type='text'
-                                placeholder='ここに旅の目的を記入'
-                                value={purpose}
-                                onChange={(e) => setPurpose(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='advice'>
-                                旅のアドバイス
-                            </label>
-                            <input
-                                id='advice'
-                                type='text'
-                                placeholder='ここに旅のアドバイスを記入'
-                                value={advice}
-                                onChange={(e) => setAdvice(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='review'>
-                                旅の感想
-                            </label>
-                            <input
-                                id='review'
-                                type='text'
-                                placeholder='ここに旅の感想を記入'
-                                value={review}
-                                onChange={(e) => setReview(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <button type="submit">きろくする</button>
-                </form>
+                        <button type="submit">きろくする</button>
+                    </form>
+                </div>    
             </div>
         </div>
     );
